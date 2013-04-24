1445af973c941ee3ff08b602a5b2245acc678564
Bob Durham <fp@hound.(none)>
2013-04-24 08:53:15 -0400

    Added source code layout to description doc

--- AirSyncServer/src/flyinpig/sync/service/CommandHandler.java
+++ AirSyncServer/src/flyinpig/sync/service/CommandHandler.java
@@ -2,19 +2,27 @@ package flyinpig.sync.service;
 
 import java.net.Socket;
 
+import flyinpig.sync.service.structures.DeviceInfo;
+
 public class CommandHandler extends Thread {
 
 	Socket sock = null;
+	DeviceInfo devinfo = null;
 	
 	public CommandHandler(Socket sock)
 	{
 		this.sock = sock;
+		devinfo = new DeviceInfo();
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		if( sock == null )
+		{
+			Main.deregister(this);
+			return;
+		}
 	}
 
 }
--- AirSyncServer/src/flyinpig/sync/service/Main.java
+++ AirSyncServer/src/flyinpig/sync/service/Main.java
@@ -11,4 +11,24 @@ public class Main {
 		
 		return;
 	}
+	
+	public static void register(CommandHandler commandHandler)
+	{
+		if( commandHandler != null )
+		{
+			devices.add(commandHandler);
+			// TODO update list in UI
+		}
+	}
+
+	public static void deregister(CommandHandler commandHandler) 
+	{
+		// TODO Auto-generated method stub
+		if( !devices.remove(commandHandler) )
+		{
+			System.err.println("Failed to remove " + commandHandler);
+		}else{
+			// TODO update list in UI
+		}
+	}
 }
