b0d25ec7b5187d42b7743de333b41de103c803a6
Bob Durham <durhamrj@gmail.com>
2013-05-06 08:12:04 -0400

    added xstream libraries, began implementing commandline interface

--- AirSyncServer/src/flyinpig/sync/service/Main.java
+++ AirSyncServer/src/flyinpig/sync/service/Main.java
@@ -1,6 +1,7 @@
 package flyinpig.sync.service;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
@@ -13,17 +14,33 @@ public class Main {
 	
 	public static LinkedList<CommandHandler> devices = new LinkedList<CommandHandler>();
 	
+	private static CommandHandler selectedDevice = null; 
+	private static String currentPath = "/";
+	private static Config config = null;
+	
 	public static void main(String[] args)
 	{
 		
 		XStream xstream = new XStream();
 		xstream.alias("config", Config.class);
-		Config config = null;
+		File f = new File("config.xml");
+		System.out.println(f.getAbsolutePath());
 		try{
-			config = (Config)xstream.fromXML("config.xml");
+			config = (Config)xstream.fromXML(f);
 		}catch( Exception e ){
 			System.err.println("Unable to find configuration file.");
 			return;
+		}
+		
+		File rootfolder = new File(config.rootpath);
+		if( !rootfolder.exists() )
+		{
+			if( !rootfolder.mkdir() )
+			{
+				System.err.println("Unable to find/create root folder.");
+				return;
+			}
+			
 		}
 //		Listener socklistener = new Listener(config.port);
 //		if( socklistener.listen() )
@@ -38,30 +55,95 @@ public class Main {
 	}
 	
 	private static void commandlineInterface() {
-		// TODO Auto-generated method stub
-		System.out.println("Enter command: ");
+		System.out.print("Enter command: ");
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
 		boolean deviceSelected = false;
 		
 		try {
 			String input = in.readLine().trim().toLowerCase();
-			while( input != "quit")
+			while( !input.equals("quit") )
 			{
-				switch(input)
+				if( input.equals("help"))
 				{
-				case "help":
 					StringBuffer sb = new StringBuffer();
-					if( deviceSelected )
+					sb.append("QUIT\t\t\t\tShutdown server and exit\n");
+					sb.append("DEVICELIST\t\t\tList connected devices\n");
+					sb.append("SELECT [deviceid]\t\tSelect a device to browse files\n");
+					sb.append("LLS\t\t\t\tList local directory contents\n");
+					sb.append("LCD\t\t\t\tChange local directory\n");
+					sb.append("AFTER SELECTING A DEVICE:\n");
+					sb.append("RLS\t\t\t\tList remote directory contents\n");
+					sb.append("RCD\t\t\t\tChange remote directory\n");
+					sb.append("PULL [remote file]\t\tRequest file from device\n");
+					sb.append("PUSH [local file]\t\tPush file to device\n");
+
+					System.out.println(sb.toString());
+				}else if(input.equals("devicelist") ){
+					if( devices.size() == 0 )
 					{
-						
+						System.out.println("No connected devices.\n");
 					}else{
-						
+						StringBuffer sb = new StringBuffer();
+						for( CommandHandler device : devices )
+						{
+							sb.append("[ ");
+							sb.append(devices.indexOf(device));
+							sb.append(" ] = ");
+							sb.append(device.devinfo);
+							sb.append("\n");
+						}
+						System.out.println(sb.toString());
 					}
-					
-					break;
-				default:
+				}else if( input.startsWith("select ") ){
+					try{
+						int selection =  Integer.parseInt(input.substring(7));
+						selectedDevice = devices.get(selection);
+						deviceSelected = true;
+					}catch(NumberFormatException e){
+						System.out.println(e.getMessage());
+					}
+				}else if( input.equals("lls") ){
+					File dir = new File(config.rootpath+currentPath);
+					StringBuffer sb = new StringBuffer(dir.getAbsolutePath());
+					sb.append("\n");
+					File[] listing = dir.listFiles();
+					sb.append(listing.length);
+					sb.append(" files/directories\n\n");
+					for( File f : dir.listFiles() )
+					{
+						sb.append(f.getName());
+						if( f.isDirectory() )
+						{
+							sb.append(File.pathSeparator);
+						}
+						sb.append("\n");
+					}
+					System.out.println(sb.toString());
+				}else if( input.startsWith("lcd ") ){
+					String folder = input.substring(5);
+					if( folder.contains("../") )
+					{
+						// TODO
+					}
+					File dir = new File(config.rootpath + folder);
+					if( !dir.exists() ){
+						dir = new File(config.rootpath + File.pathSeparator + folder);
+					}
+					if( !dir.exists() ){
+						System.err.println("Folder does not exist.\n");
+					}else{
+						// dir exists and is our new folder
+					}
+				}else if( input.equals("rls") ){
+					// TODO
+				}else if( input.equals("rcd") ){
+					// TODO
+				}else{
 					System.out.println("Unrecognized command.\n");
 				}
+				
+				System.out.print("Enter command: ");
+				input = in.readLine().trim().toLowerCase();
 			}
 			
 		} catch (IOException e) {
---
+++ AirSyncServer/src/flyinpig/sync/service/structures/Config.java
@@ -0,0 +1,6 @@
+package flyinpig.sync.service.structures;
+
+public class Config {
+	public int port;
+	public String rootpath;
+}
